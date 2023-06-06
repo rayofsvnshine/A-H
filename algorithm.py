7368ec64b67f1fd8700b1ac1dcda714beb95cada
Robert <mailroberthardy@gmail.com>
2023-06-06 15:37:42 +0200

    main is now algorithm

---
+++ algorithm.py
@@ -0,0 +1,46 @@
+"""
+* Imports the protein from the proteins.csv file
+* Puts protein in Protein class
+* Output of Folder in Scoring class
+* Exports results to the output.csv file
+"""
+
+# Import classes and used libraries
+from classes.folded_protein import Fold
+from classes.properties import Protein
+from classes.protein_folder import Folder
+from classes.scoring import Score
+from sys import argv
+import csv
+
+def import_protein(protein_number: int) -> bool:
+    """Loads the selected protein into the protein class."""
+
+    # Load file
+    with open('input/proteins.csv') as csvfile:
+
+        # Read file
+        proteins = csv.reader(csvfile, delimiter=',')
+
+        # Select protein
+        for row in proteins:
+            if row[0] == protein_number:
+                Protein.protein = row[1]
+                return True
+            else:
+                return False
+
+
+if __name__ == "__main__":
+
+    # Check command line arguments
+    if len(argv) != 2:
+        print("Usage: python main.py <protein number>")
+        exit(1)
+
+    # Check if protein is in the file
+    if not import_protein(argv[1]):
+        print("Protein not found")
+
+    # Import selected protein
+    import_protein(argv[1])
