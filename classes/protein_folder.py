8978d4e6280e371c2f3ab359206a370d238ed6b6
Ray Pelupessy <tatum44@outlook.com>
2023-06-06 12:47:05 +0200

    started on Folder

--- classes/protein_folder.py
+++ classes/protein_folder.py
@@ -22,8 +22,42 @@ List of tuples with directions
 * checks for hydrophobic aminoacids (H) if there are other H's around (without covalent binding)
     * if there is an H around, calls scoring class to keep track of score 
 """
+from random import choice
+# import Fold
 
 class Folder:
 
-    def __init__(self):
-        self.thing = 0
+    def __init__(self, Protein):
+        self.Protein = Protein
+        self.Grid = self.make_grid()
+        
+    def make_folds(self):
+        # stop making folds after 5 valid folds are found
+        valid_folds = []
+        
+        while len(valid_folds) < 4:
+            new_fold = self.fold_protein()
+            valid_folds.append(new_fold)
+        
+    def fold_protein(self):
+        # needs to return a fold
+            
+        # make grid
+        starting_point = (0,0)
+        # put aminoacids down until end of protein
+        for aminoacid in self.Protein.length:
+            options = self.check_directions(starting_point)
+            starting_point = choice(options)
+            
+    def make_grid(self):
+        gridspace = []
+        for y in len(self.Protein.length):
+            gridspace.append([])
+            for x in len(self.Protein.length):
+                coordinate = ((0 - y), (0 - x))
+                gridspace[y].append(coordinate)
+            
+    def check_direction(self, starting_point):
+        # checks which directions line can go
+        # returns list of possible coordinates
+        pass
