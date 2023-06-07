323922f733c5d0e67d0f9ae977223604c11d9a67
Robert <mailroberthardy@gmail.com>
2023-06-08 00:52:05 +0200

    updated main

--- main.py
+++ main.py
@@ -6,17 +6,35 @@
 """
 
 # Import classes and used libraries
-from classes.folded_protein import Fold
+# from classes.folded_protein import Fold
 from classes.properties import Protein
-from classes.protein_folder import Folder
-from classes.scoring import Score
+# from classes.protein_folder import Folder
+# from classes.scoring import Score
 from sys import argv
 import csv
+
+
+def select_protein():
+    """Gives an overview of the available proteins in the csv file."""
+
+    with open("proteins.csv", "r") as csvfile:
+
+        proteins = [row for row in csv.reader(csvfile)]
+
+        if len(proteins) == 0:
+            return False
+
+        overview = "\n"
+        for row in proteins:
+            overview += "   ".join(row) + "\n"
+
+    return overview
+
 
 def import_protein(protein_number: int) -> bool:
     """Loads the selected protein into the protein class."""
 
-    with open("input/proteins.csv", "r") as csvfile:
+    with open("proteins.csv", "r") as csvfile:
 
         # Read input file
         proteins = csv.reader(csvfile, delimiter=',')
@@ -32,7 +50,7 @@ def import_protein(protein_number: int) -> bool:
 def export_protein(foldingsteps, score):
     """Exports results to the output.csv file."""
 
-    with open('output/output.csv', 'w') as csvfile:
+    with open('output.csv', 'w') as csvfile:
 
         # Create output file
         output = csv.writer(csvfile)
@@ -47,36 +65,40 @@ def export_protein(foldingsteps, score):
         # Write score
         output.writerow(["score", score])
 
-        # OTHER WAY WITH DICTWRITER:
-
-        # # Create two columns
-        # output = csv.DictWriter(csvfile, fieldnames = ["amino", "fold"])
-        # output.writeheader()
-
-        # # Write folding data
-        # for step in foldingsteps:
-        #     output.writerow({"amino": step[0], "fold": step[1]})
-
-        # # Write score
-        # output.writerow({"amino": "score", "fold": score})
-
 
 if __name__ == "__main__":
 
     # Check command line arguments
-    if len(argv) != 2:
+    if len(argv) >= 3:
         print("Usage: python main.py <protein number>")
         exit(1)
 
     # Import protein if found
-    if not import_protein(argv[1]):
-        print("Protein not found")
-        exit(1)
+    elif len(argv) == 2:
+        if not import_protein(argv[1]):
+            print("Protein not found")
+            exit(1)
 
-    # Test protein import
-    print(Protein.protein)
+    # Show proteins from csv file if found
+    elif len(argv) == 1:
+        if not select_protein():
+            print("No proteins found in proteins.csv")
+            exit(1)
+        print(select_protein())
+
+        # Ask user to select a protein
+        protein_number = input("Select a protein: ")
+
+        # Import protein if found
+        if not import_protein(protein_number):
+            print("Selected protein not found")
+            exit(1)
+
+    # Check the selected protein
+    print(f"Selected protein: {Protein.protein}")
 
     # Test result export
     foldingsteps = [("H", 1), ("H", 2), ("P", -1), ("H", -1), ("P", 2), ("P", 2), ("P", 1), ("P", -2), ("H", 0)]
     score = -2
     export_protein(foldingsteps, score)
+    print("Results can be found in output")
