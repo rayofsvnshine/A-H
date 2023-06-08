a9e285111cfaffd3d6df4d046a8bbd5068ad7a51
Vera Spek <vera@sp-byods-145-109-36-106.wireless.uva.nl>
2023-06-08 12:17:52 +0200

    get_score

--- classes/scoring.py
+++ classes/scoring.py
@@ -26,27 +26,57 @@ class Score:
         given coordinate of the H aminoacid sides another H, if so, -1 is added to the score.
         Returns the score retrieved from this aminoacid."""
         score = 0
+        last_coordinate = None
+        current_coordinate = None
+        next_coordinate = None
+        index = 0
+        for coordinate, index in coordinates:
+            current_coordinate = coordinate
+            for n_coordinate + 1 in coordinates:
+                next_coordinate = n_coordinate
+                if index != 0:
+                    if last_coordinate[0] > current_coordinate[0]:  # hij komt van rechts
+                        if next_coordinate[0] < current_coordinate[0]: # hij gaat naar rechts
+                            if current_coordinate[1] + 1 in coordinates:
+                                score += -1
+                            if current_coordinate[1] - 1 in coordinates:
+                                score += -1
+                        if next_coordinate[1] > current_coordinate[1]: # hij gaat naar beneden
+                            if current_coordinates[0] + 1 in coordinates:
+                                score += -1
+                            if current_coordinate[1] + 1 in coordinates:
+                                score += -1
+                        if next_coordinate[1] < current_coordinate[1]: # hij gaat naar beneden
+                            if current_coordinate[0] + 1 in coordinates:
+                                score += -1
+                            if current_coordinate[1] + 1 in coordinates
 
-        for coordinate in coordinates:
-            
+                    if last_coordinate[0] < current_coordinate[0]: # hij komt van links
+                        if next_coordinate[0] > current_coordinate[0]: # hij gaat naar links
+                            if current_coordinates[1] - 1 in coordinates:
+                                score += -1
+                            if current_coordinates[1] + 1 in coordinates:
+                                score += -1 
+                        if next_coordinate[1] > current_coordinate[1]: # hij gaat naar beneden
+                            if current_coordinates[0] + 1 in coordinates:
+                                score += -1
+                        if next_coordinate[1] < current_coordinate[1]: # hij gaat naar boven
+                            if current_coordinates[0] + 1 in coordinates:
+                                score += -1
 
+                    if last_coordinate[1] > current_coordinate [1]: # hij komt van onder
+                        if next_coordinate[0] < current_coordinate[0]:
+                        if next_coordinate[0] > current_coordinate[0]:
+                        if next_coordinate[1] < current_coordinate[1]:
 
+                    if last_coordinate[1] < current_coordinate [1]: # hij komt van boven
+                        if next_coordinate[0] < current_coordinate[0]:
+                        if next_coordinate[0] > current_coordinate[0]:
+                        if next_coordinate[1] > current_coordinate[1]:
+            else:
+                continue
 
-        # for coordinate in coordinates:
-        #     if "2" in last_chosen_direction:
-        #         if "-" in last_chosen_direction:
-        #             if coordinate == current_coordinate[0] - 1 or coordinate == current_coordinate[0] + 1 or coordinate == current_coordinate[1] - 1:
-        #             score += -1
-        #         else:
-        #             if coordinate == current_coordinate[0] - 1 or coordinate == current_coordinate[0] + 1 or coordinate == current_coordinate[1] + 1:
-        #                 score += -1
-        #     elif "1" in last_chosen_direction:
-        #         if "-" in last_chosen_direction:
-        #             if coordinate == current_coordinate[1] - 1 or coordinate == current_coordinate[1] + 1 or coordinate == current_coordinate[1] - 1:
-        #                 score += -1
-        #         else:
-        #             if coordinate == current_coordinate[1] - 1 or coordinate == current_coordinate[1] + 1 or coordinate == current_coordinate[1] + 1:
-        #                 score += -1
+        last_coordinate = coordinate
 
         return score
 
