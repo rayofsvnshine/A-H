d10877205171a618dc335d04a23ee49b4d30ea68
Robert <mailroberthardy@gmail.com>
2023-06-08 12:18:48 +0200

    minor changes

--- __init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Allows direct importation of classes to main.py
-"""
-
-from .folded_protein import Fold
-from .properties import Protein
-from .protein_folder import Folder
-from .scoring import Score
